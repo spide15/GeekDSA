2e6286f1e57765d73340bfd1cccf24c5d7f5b10a
spide15 <131018851+spide15@users.noreply.github.com>
2023-08-14 21:30:43 +0530

    14Aug

---
+++ potd-14Aug23/Non Repeating Numbers.java
@@ -0,0 +1,65 @@
+// Given an array A containing 2*N+2 positive numbers, out of which 2*N numbers exist in pairs whereas the other two number occur exactly once and are distinct. Find the other two numbers. Return in increasing order.
+
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            String s = br.readLine();
+            String[] S = s.split(" ");
+            int[] v = new int[2 * n + 2];
+            for(int i = 0; i < 2 * n + 2; i++)
+            {
+                v[i] = Integer.parseInt(S[i]);
+            }
+            Solution ob = new Solution();
+            int[] ans = ob.singleNumber(v);
+            for(int i = 0; i < ans.length; i++)
+                System.out.print(ans[i] + " ");
+            System.out.println();
+        }
+    }
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public int[] singleNumber(int[] nums)
+    {
+        // Code here
+        // Arrays.sort(nums);
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        }
+        
+        int[] result=new int[2];
+        int i=0;
+        for(int num:map.keySet()){
+            if(map.get(num)==1){
+                result[i++]=num;
+            }
+        }
+        if (result[0] > result[1]) {
+            int temp = result[0];
+            result[0] = result[1];
+            result[1] = temp;
+        }
+        return result;
+    }
+}
