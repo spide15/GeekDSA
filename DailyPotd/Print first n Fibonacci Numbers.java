c5ba71e88032fdf7895495cc2a8ce2989d78020c
spide15 <131018851+spide15@users.noreply.github.com>
2023-09-17 12:31:38 +0530

    17Sept

---
+++ DailyPotd/Print first n Fibonacci Numbers.java
@@ -0,0 +1,63 @@
+// Given a number N, find the first N Fibonacci numbers. The first two number of the series are 1 and 1.
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+class GFG {
+	public static void main (String[] args) {
+	    
+	    //taking input using Scanner class
+		Scanner sc=new Scanner(System.in);
+		
+		//taking total count of testcases
+		int t=sc.nextInt();
+		
+		while(t-->0)
+		{
+		    //taking total number of elements
+		    int n=sc.nextInt();
+		    
+		    //calling printFibb() method
+		    long[] res = new Solution().printFibb(n);
+		    
+		    //printing the elements of the array
+		    for (int i = 0; i < res.length; i++)
+		        System.out.print (res[i]+" ");
+		    System.out.println();
+		}
+	}
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution
+{
+    //Function to return list containing first n fibonacci numbers.
+    public static long[] printFibb(int n) 
+    {
+        //Your code here
+        
+        long[] dp=new long[n];
+        // dp[0]=1;
+        dp[0]=1;
+        if(n==1)
+        return dp;
+        
+        dp[1]=1;
+        if(n==2){
+        return dp;
+        }
+        for(int i=2;i<n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+            
+        }
+        return dp;
+    }
+}
