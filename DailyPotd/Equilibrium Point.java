379142bae4205a861fdd4d543627d2d699572514
spide15 <131018851+spide15@users.noreply.github.com>
2023-09-23 14:13:04 +0530

    23Sept

---
+++ DailyPotd/Equilibrium Point.java
@@ -0,0 +1,59 @@
+// Given an array A of n positive numbers. The task is to find the first equilibrium point in an array. Equilibrium point in an array is a position such that the sum of elements before it is equal to the sum of elements after it.
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+
+class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while (t-- > 0) {
+            
+            //taking input n
+            int n = Integer.parseInt(br.readLine().trim());
+            long arr[] = new long[n];
+            String inputLine[] = br.readLine().trim().split(" ");
+            
+            //adding elements to the array
+            for (int i = 0; i < n; i++) {
+                arr[i] = Long.parseLong(inputLine[i]);
+            }
+
+            Solution ob = new Solution();
+            
+            //calling equilibriumPoint() function
+            System.out.println(ob.equilibriumPoint(arr, n));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+
+    
+    // a: input array
+    // n: size of array
+    // Function to find equilibrium point in the array.
+    public static int equilibriumPoint(long arr[], int n) {
+
+        // Your code here
+        int leftSum=0;
+        int totalSum=0;
+        for(int i=0;i<n;i++){
+            totalSum+=arr[i];
+        }
+        for(int i=0;i<n;i++){
+            totalSum-=arr[i];
+            if(leftSum==totalSum){
+                return i+1;
+            }
+            leftSum+=arr[i];
+        }
+        return -1;
+    }
+}
