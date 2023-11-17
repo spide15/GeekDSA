d4601a4ccfb5f06ec2a3dd822e3fae8ab4e6056b
spide15 <131018851+spide15@users.noreply.github.com>
2023-11-17 20:34:05 +0530

    17nov23

---
+++ DailyPotd/Binary Tree to CDLL.java
@@ -0,0 +1,180 @@
+// Given a Binary Tree of N edges. The task is to convert this to a Circular Doubly Linked List (CDLL) in-place. The left and right pointers in nodes are to be used as previous and next pointers respectively in CDLL. The order of nodes in CDLL must be same as Inorder of the given Binary Tree. The first node of Inorder traversal (left most node in BT) must be head node of the CDLL
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+class GfG {
+    
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+                
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    static void printInorder(Node root)
+    {
+        if(root == null)
+            return;
+            
+        printInorder(root.left);
+        System.out.print(root.data+" ");
+        
+        printInorder(root.right);
+    }
+    static void displayCList(Node head)
+    {
+        Node itr = head;
+        do
+        {
+            System.out.print(itr.data + " ");
+            itr = itr.right;
+        } while (head!=itr);
+        System.out.println();
+    	itr=itr.left;
+    	head = itr;
+    	do{
+    		System.out.print(itr.data + " ");
+    		itr=itr.left;
+    	}while(head!=itr);
+    	System.out.println();
+    }
+    
+	public static void main (String[] args) throws IOException {
+	        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+	        
+	        int t=Integer.parseInt(br.readLine());
+	        while(t-- > 0){
+	            String s= br.readLine();
+	            Node root = buildTree(s);
+	            
+	            Solution boj = new Solution();
+	            Node head = boj.bTreeToClist(root);
+	            displayCList(head);
+	            
+	        }
+	    
+	}
+}
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+
+//User function Template for Java
+/*
+Node defined as
+class Node{
+    int data;
+    Node left,right;
+    Node(int d){
+        data=d;
+        left=right=null;
+    }
+}
+*/
+class Solution
+{ 
+    //Function to convert binary tree into circular doubly linked list.
+    Node bTreeToClist(Node root)
+    {
+        //your code here
+         Stack<Node> st=new Stack<>();
+        addToStack(root,st);
+        Node head,cur;
+        Node temp=st.pop();
+        addToStack(temp.right,st);
+        head=temp;
+        cur=head;
+        while(!st.isEmpty()){
+            temp=st.pop();
+            addToStack(temp.right,st);
+            cur.right=temp;
+            temp.left=cur;
+            cur=cur.right;
+        }
+        cur.right=head;
+        head.left=cur;
+        return head;
+    }
+    static void addToStack(Node root,Stack<Node> st){
+        if(root==null)
+        return;
+        st.add(root);
+        while(root.left!=null){
+            st.add(root.left);
+            root=root.left;
+        }
+    }
+    
+}
+    
