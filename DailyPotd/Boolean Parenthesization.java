964b249038b30d9bc66e1b3e71459d97a26b1b69
spide15 <131018851+spide15@users.noreply.github.com>
2024-02-21 13:17:16 +0530

    21Feb24

---
+++ DailyPotd/Boolean Parenthesization.java
@@ -0,0 +1,70 @@
+// Given a boolean expression s of length n with following symbols.
+// Symbols
+//     'T' ---> true
+//     'F' ---> false
+// and following operators filled between symbols
+// Operators
+//     &   ---> boolean AND
+//     |   ---> boolean OR
+//     ^   ---> boolean XOR
+// Count the number of ways we can parenthesize the expression so that the value of expression evaluates to true.
+
+// Note: The answer can be large, so return it with modulo 1003
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+        while(t-- > 0){
+            int N = Integer.parseInt(in.readLine());
+            String S = in.readLine();
+            
+            Solution ob = new Solution();
+            System.out.println(ob.countWays(N, S));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+      static int countWays(int n, String s) {
+        int mod = 1003;
+        int[][][] dp = new int[n][n][2];
+        for (int i = 0; i < n; i += 2) {
+            if (s.charAt(i) == 'T')dp[i][i][1] = 1;
+            else dp[i][i][0] = 1;
+        }
+
+        for (int cut = 2; cut < n; cut += 2) {
+            for (int i = 0; i < n - cut; i += 2) {
+                int j = i + cut;
+                for (int k = i + 1; k < j; k += 2) {
+                    
+                    int leftTrue = dp[i][k-1][1],leftFalse = dp[i][k-1][0],rightTrue = dp[k+1][j][1],rightFalse = dp[k+1][j][0];
+                    
+                    if (s.charAt(k) == '&') {
+                        dp[i][j][1] = (dp[i][j][1] + (leftTrue * rightTrue) % mod) % mod;
+                        dp[i][j][0] = (dp[i][j][0] + ((leftFalse * rightFalse) % mod+ (leftFalse * rightTrue) % mod+ (leftTrue * rightFalse) % mod) % mod) % mod;
+                    } else if (s.charAt(k) == '|') {
+                        dp[i][j][0] = (dp[i][j][0] + (leftFalse * rightFalse) % mod) % mod;
+                        dp[i][j][1] = (dp[i][j][1] + ((leftTrue * rightTrue) % mod+ (leftFalse * rightTrue) % mod+ (leftTrue * rightFalse) % mod) % mod) % mod;
+                    } else if (s.charAt(k) == '^') {
+                        dp[i][j][1] = (dp[i][j][1] + ((leftTrue * rightFalse) % mod + (leftFalse * rightTrue) % mod) % mod) % mod;
+                        dp[i][j][0] = (dp[i][j][0] + ((leftTrue * rightTrue) % mod+ (leftFalse * rightFalse) % mod) % mod) % mod;
+                    }
+                }
+            }
+        }
+
+        return dp[0][n - 1][1];
+      }
+}
