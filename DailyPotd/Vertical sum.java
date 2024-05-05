830dbecd2a99c2e833c7a565acdfd64ed8bc9787
spide15 <131018851+spide15@users.noreply.github.com>
2024-05-05 23:07:59 +0530

    5May24

---
+++ DailyPotd/Vertical sum.java
@@ -0,0 +1,138 @@
+// Given a binary tree having n nodes, find the vertical sum of the nodes that are in the same vertical line. Return all sums through different vertical lines starting from the left-most vertical line to the right-most vertical line.
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+class GfG {
+    
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    
+	public static void main (String[] args) throws IOException{
+	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        
+	        int t=Integer.parseInt(br.readLine());
+    
+	        while(t > 0){
+	            String s = br.readLine();
+    	    	Node root = buildTree(s);
+        	    Solution g = new Solution();
+			
+			    ArrayList <Integer> res = g.verticalSum(root);
+			    for (Integer num : res) System.out.print (num + " ");
+			    System.out.println();
+                t--;
+            
+        }
+    }
+  
+}
+
+
+// } Driver Code Ends
+
+
+/*Complete the function below
+Node is as follows:
+class Node{
+int data;
+Node left, right;
+Node(int item)
+{
+    data = item;
+    left = right = null
+}
+}
+*/
+class Solution{
+    public ArrayList <Integer> verticalSum(Node root) {
+        // add your code here
+        ArrayList<Integer> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        TreeMap<Integer, Integer> verticalSumMap = new TreeMap<>();
+        verticalSumUtil(root, 0, verticalSumMap);
+        for (Map.Entry<Integer, Integer> entry : verticalSumMap.entrySet()) {
+            result.add(entry.getValue());
+        }
+        return result;
+    }
+    private void verticalSumUtil(Node root, int verticalLine, TreeMap<Integer, Integer> verticalSumMap) {
+        if (root == null) {
+            return;
+        }
+        verticalSumMap.put(verticalLine, verticalSumMap.getOrDefault(verticalLine, 0) + root.data);
+        verticalSumUtil(root.left, verticalLine - 1, verticalSumMap);
+        verticalSumUtil(root.right, verticalLine + 1, verticalSumMap);
+    }
+}
