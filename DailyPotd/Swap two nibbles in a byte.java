2658cbd96a7f243e3aa14df766c9fd211080d605
spide15 <131018851+spide15@users.noreply.github.com>
2024-05-31 22:19:51 +0530

    31May24

---
+++ DailyPotd/Swap two nibbles in a byte.java
@@ -0,0 +1,31 @@
+// Given a number n, Your task is to swap the two nibbles and find the resulting number. 
+
+// A nibble is a four-bit aggregation, or half an octet. There are two nibbles in a byte. For example, the decimal number 150 is represented as 10010110 in an 8-bit byte. This byte can be divided into two nibbles: 1001 and 0110.
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            int n = Integer.parseInt(read.readLine());
+            Solution ob = new Solution();
+            System.out.println(ob.swapNibbles(n));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+class Solution {
+    static int swapNibbles(int n) {
+        // code here
+         int first=n%16;  //FIRST NIBBLE
+        int second=n/16; //SECOND NIBBLE
+        return first*16+second;
+    }
+}
