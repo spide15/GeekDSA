cbe7460aa3e71bcd46b60871577d0359130ecc8f
spide15 <131018851+spide15@users.noreply.github.com>
2024-06-11 23:27:06 +0530

    11June24

---
+++ DailyPotd/Maximum Tip Calculator.java
@@ -0,0 +1,102 @@
+// In a restaurant, two waiters, A and B, receive n orders per day, earning tips as per arrays arr[i] and brr[i] respectively. If A takes the ith order, the tip is arr[i] rupees; if B takes it, the tip is brr[i] rupees.
+
+// To maximize total tips, they must distribute the orders such that:
+
+// A can handle at most x orders
+// B can handle at most y orders
+// Given that x + y ≥ n, all orders can be managed by either A or B. Return the maximum possible total tip after processing all the orders.
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class IntArray {
+    public static int[] input(BufferedReader br, int n) throws IOException {
+        String[] s = br.readLine().trim().split(" ");
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) a[i] = Integer.parseInt(s[i]);
+
+        return a;
+    }
+
+    public static void print(int[] a) {
+        for (int e : a) System.out.print(e + " ");
+        System.out.println();
+    }
+
+    public static void print(ArrayList<Integer> a) {
+        for (int e : a) System.out.print(e + " ");
+        System.out.println();
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+
+            int n;
+            n = Integer.parseInt(br.readLine());
+
+            int x;
+            x = Integer.parseInt(br.readLine());
+
+            int y;
+            y = Integer.parseInt(br.readLine());
+
+            int[] arr = IntArray.input(br, n);
+
+            int[] brr = IntArray.input(br, n);
+
+            Solution obj = new Solution();
+            long res = obj.maxTip(n, x, y, arr, brr);
+
+            System.out.println(res);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+class Solution {
+    static class Pair{
+        int a;
+        int b;
+        int diff;
+        Pair(int a , int b , int diff){
+            this.a = a;
+            this.b = b;
+            this.diff = diff;
+        }
+    }
+    
+    public static long maxTip(int n, int x, int y, int[] arr, int[] brr) {
+        PriorityQueue<Pair>pq = new PriorityQueue<>((a,b)-> b.diff - a.diff);
+        
+        for(int i =0;i<n;i++){
+            pq.add(new Pair(arr[i] , brr[i] , Math.abs(arr[i]-brr[i])));
+        }
+        
+        long sum = 0;
+        int countA =0;
+        int countB = 0;
+        while(!pq.isEmpty() ){
+            Pair p = pq.poll();
+            int a = p.a;
+            int b = p.b;
+            
+            if((a>=b && countA<x) || countB>=y){
+                sum+=a;
+                countA++;
+            }
+            else{
+                sum+=b;
+                countB++;
+            }
+        }
+        return sum;
+    }
+}
