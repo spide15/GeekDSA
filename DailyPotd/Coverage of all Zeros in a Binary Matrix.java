c234e8d2f9302a28b72186a2f4c2854bc6359900
spide15 <131018851+spide15@users.noreply.github.com>
2024-06-26 23:27:36 +0530

    26June24

---
+++ DailyPotd/Coverage of all Zeros in a Binary Matrix.java
@@ -0,0 +1,56 @@
+// Given a binary matrix contains 0s and 1s only, we need to find the sum of coverage of all zeros of the matrix where coverage for a particular 0 is defined as a total number of ones around a zero in left, right, up and bottom directions.
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            String[] s = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(s[0]);
+            int m = Integer.parseInt(s[1]);
+            int[][] matrix = new int[n][m];
+            for (int i = 0; i < n; i++) {
+                String[] S = br.readLine().split(" ");
+                for (int j = 0; j < m; j++) {
+                    matrix[i][j] = Integer.parseInt(S[j]);
+                }
+            }
+            Solution ob = new Solution();
+            int ans = ob.findCoverage(matrix);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public int findCoverage(int[][] mat) {
+        // code here
+          int c=0;
+        for(int i=0;i<mat.length;i++){
+            for(int j=0;j<mat[0].length;j++){
+                if(mat[i][j]==0){
+                if(i-1>-1&&mat[i-1][j]==1)
+                c++;
+                if(i+1<mat.length&&mat[i+1][j]==1)
+                c++;
+                if(j-1>-1&&mat[i][j-1]==1)
+                c++;
+                 if(j+1<mat[0].length&&mat[i][j+1]==1)
+                c++;
+                }
+        }
+        }
+        return c;
+    }
+}
