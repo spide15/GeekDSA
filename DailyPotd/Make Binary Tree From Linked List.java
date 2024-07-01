62599a60daf61bcac0c20e5ef1d238e7264e4630
spide15 <131018851+spide15@users.noreply.github.com>
2024-07-01 13:28:23 +0530

    1July24

---
+++ DailyPotd/Make Binary Tree From Linked List.java
@@ -0,0 +1,130 @@
+// Given a Linked List Representation of Complete Binary Tree. The task is to construct the Binary tree and print the level order traversal of the Binary tree. 
+// Note: The complete binary tree is represented as a linked list in a way where if the root node is stored at position i, its left, and right children are stored at position 2*i+1, and 2*i+2 respectively. H is the height of the tree and this space is used implicitly for the recursion stack.
+//{ Driver Code Starts
+import java.util.*;
+class Tree {
+    int data;
+    Tree left;
+    Tree right;
+    Tree(int d) {
+        data = d;
+        left = null;
+        right = null;
+    }
+} 
+
+class Node {
+    int data;
+    Node next;
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+} 
+
+class MakeBT {
+    static Node lhead;
+    static Queue<Tree> queue = new LinkedList<Tree>();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            Tree node = null;
+            lhead = null;
+            Node head = null;
+            while (!queue.isEmpty()) {
+                queue.poll();
+            }
+            int n = sc.nextInt();
+            if (n != 0) {
+                MakeBT llist = new MakeBT();
+                int a1 = sc.nextInt();
+                head = new Node(a1);
+                llist.addToTheLast(head);
+                lhead = head;
+                for (int i = 1; i < n; i++) {
+                    int a = sc.nextInt();
+                    llist.addToTheLast(new Node(a));
+                }
+            }
+            GfG g = new GfG();
+            Tree root = g.convert(lhead, node);
+            try {
+                levelOrder(root, n);
+            } catch (NullPointerException ex) {
+                System.out.println(-1);
+            }
+            System.out.println();
+        }
+    }
+    public static void levelOrder(Tree root, int n) {
+        queue.add(root);
+        while (!queue.isEmpty() && n-- > 0) {
+            Tree proot = queue.remove();
+            try {
+                queue.add(proot.left);
+                queue.add(proot.right);
+                System.out.print(proot.data + " ");
+            } catch (NullPointerException ex) {
+            }
+        }
+    }
+    public void addToTheLast(Node head) {
+        if (lhead == null)
+            lhead = head;
+        else {
+            Node temp = lhead;
+            while (temp.next != null) temp = temp.next;
+            temp.next = head;
+        }
+    }
+}
+// } Driver Code Ends
+
+
+/*class Tree{
+int data;
+    Tree left;
+    Tree right;
+    Tree(int d){
+        data=d;
+        left=null;
+        right=null;
+    }
+}
+
+class Node {
+    int data;
+    Node next;
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+}*/
+
+
+class GfG 
+{
+    //Function to make binary tree from linked list.
+    public static Tree convert(Node head, Tree node) {
+        // add code here.}
+        if(head==null)
+        return null;
+        
+        List<Tree> nodes=new ArrayList<>();
+        Node current=head;
+        while(current!=null){
+            nodes.add(new Tree(current.data));
+            current=current.next;
+        }
+        for(int i=0;i<nodes.size();i++){
+            if(2*i+1<nodes.size()){
+                nodes.get(i).left=nodes.get(2*i+1);
+            }
+            if(2*i+2<nodes.size()){
+                nodes.get(i).right=nodes.get(2*i+2);
+            }
+        }
+        return nodes.get(0);
+    }
+}  
