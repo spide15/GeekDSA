47150bf94191d0c7bc12814196b232087e332189
spide15 <131018851+spide15@users.noreply.github.com>
2024-07-17 21:47:43 +0530

    17July24

---
+++ DailyPotd/Construct Binary Tree from Parent Array.java
@@ -0,0 +1,120 @@
+// Given an array parent that is used to represent a tree. The array indices (0-based indexing) are the values of the tree nodes and parent[i] denotes the parent node of a particular node. The parent of the root node would always be -1, as there is no parent for the root. Construct the standard linked representation of Binary Tree from this array representation and return the root node of the constructed tree.
+
+// Note: If two elements have the same parent, the one that appears first in the array will be the left child and the other is the right child. You don't need to print anything, the driver code will print the level order traversal of the returned root node to verify the output.
+//{ Driver Code Starts
+import java.lang.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node left, right;
+
+    Node(int key) {
+        data = key;
+        left = right = null;
+    }
+}
+
+class CreateTree {
+    static ArrayList<Integer> result = new ArrayList<Integer>();
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int arr[] = new int[n];
+
+            for (int i = 0; i < n; i++) arr[i] = sc.nextInt();
+
+            Solution ob = new Solution();
+            Node root = ob.createTree(arr);
+
+            printLevelOrder(root);
+            System.out.println();
+        }
+    }
+
+    public static void printList(Node root) {
+        while (root != null) {
+            System.out.print(root.data + " ");
+        }
+    }
+
+    public static void printLevelOrder(Node root) {
+        int h = height(root);
+        int i;
+        for (i = 1; i <= h; i++) {
+            result.clear();
+            printGivenLevel(root, i);
+            Collections.sort(result);
+            for (int j = 0; j < result.size(); j++)
+                System.out.print(result.get(j) + " ");
+        }
+    }
+
+    public static int height(Node root) {
+        if (root == null)
+            return 0;
+
+        else {
+
+            int lheight = height(root.left);
+            int rheight = height(root.right);
+
+            if (lheight > rheight)
+                return (lheight + 1);
+            else
+                return (rheight + 1);
+        }
+    }
+
+    public static void printGivenLevel(Node node, int level) {
+        if (node == null) return;
+        if (level == 1)
+            result.add(node.data);
+        else if (level > 1) {
+            printGivenLevel(node.left, level - 1);
+            printGivenLevel(node.right, level - 1);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+/*node class of the binary tree
+class Node
+{
+    int data;
+    Node left, right;
+    Node(int key)
+    {
+        data = key;
+        left = right = null;
+    }
+}*/
+class Solution {
+    // Function to construct binary tree from parent array.
+    public Node createTree(int parent[]) {
+        // Your code here
+        Node root=null;
+        Node tree[]=new Node[parent.length];
+        for(int i=0;i<parent.length;i++){
+            tree[i]=new Node(i);
+        }
+        for(int i=0;i<parent.length;i++){
+            if(parent[i]==-1){
+                root=tree[i];
+            }else{
+                if(tree[parent[i]].left==null)
+                tree[parent[i]].left=tree[i];
+                else
+                tree[parent[i]].right=tree[i];
+            }
+        }
+        return root;
+    }
+}
+
