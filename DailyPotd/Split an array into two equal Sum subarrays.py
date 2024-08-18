19d83859a3b0e6687331a83f8c31dfaa4b8e5928
spide15 <131018851+spide15@users.noreply.github.com>
2024-08-18 20:28:37 +0530

    18Aug24

---
+++ DailyPotd/Split an array into two equal Sum subarrays.py
@@ -0,0 +1,35 @@
+# Given an array of integers arr, return true if it is possible to split it in two subarrays (without reordering the elements), such that the sum of the two subarrays are equal. If it is not possible then return false.
+class Solution:
+    def canSplit(self, arr):
+        #code here
+        s=sum(arr)
+        t=0
+        for i in arr:
+            s-=i
+            t+=i
+            if t==s:
+                return True
+        return False
+
+
+#{ 
+ # Driver Code Starts
+if __name__ == "__main__":
+    import sys
+    input = sys.stdin.read
+    data = input().splitlines()
+
+    t = int(data[0])
+    index = 1
+    for _ in range(t):
+        arr = list(map(int, data[index].split()))
+        index += 1
+
+        obj = Solution()
+        res = obj.canSplit(arr)
+        if (res):
+            print("true")
+        else:
+            print("false")
+
+# } Driver Code Ends
